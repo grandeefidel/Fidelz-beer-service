59e274c97e55e95e3f41985d972845bda06111e0
kechinaka <kechinaka@unionbankng.com>
2022-06-04 22:03:46 +0100

    initial project add. Closes #1

---
+++ src/main/java/com/fidelivery/fidelzbeerservice/FidelzBeerServiceApplication.java
@@ -0,0 +1,13 @@
+package com.fidelivery.fidelzbeerservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FidelzBeerServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FidelzBeerServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/fidelivery/fidelzbeerservice/FidelzBeerServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fidelivery.fidelzbeerservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FidelzBeerServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
